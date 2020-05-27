b0ca9866c83356f866786f76a41ac0bad648726a
rahul <rahul@nextazy.com>
2020-05-28 00:45:02 +0530

    updates

--- fill_erc20.js
+++ fill_erc20.js
@@ -17,22 +17,35 @@ let utils = require('./utils');
     const TestBTokenAddress = "0x33024A622305ca41fDD24C74B84Be23cd4D9F7bB".toLowerCase()
     const TestCTokenAddress = "0xB74F204A81702FB85d66B4C1Cf0873c61d665f25".toLowerCase();
     const makerAssetAmount = Web3Wrapper.toBaseUnitAmount(new BigNumber(500), DECIMALS);
-    const takerAssetAmount = Web3Wrapper.toBaseUnitAmount(new BigNumber(500), DECIMALS);
+    const takerAssetAmount = Web3Wrapper.toBaseUnitAmount(new BigNumber(0.5), DECIMALS);
     const makerAssetData = await contractWrappers.devUtils.encodeERC20AssetData(TestBTokenAddress).callAsync();
     const takerAssetData = await contractWrappers.devUtils.encodeERC20AssetData(TestCTokenAddress).callAsync();
     let txHash;
 
-    const erc20Token = new ERC20TokenContract(TestBTokenAddress, providerEngine());
-    const makerTestBApprovalTxHash = await erc20Token
-        .approve(contractWrappers.contractAddresses.erc20Proxy, UNLIMITED_ALLOWANCE_IN_BASE_UNITS)
-        .sendTransactionAsync({ from: maker, gasPrice: 0 });
+    // const erc20Token = new ERC20TokenContract(TestBTokenAddress, providerEngine());
+    // const makerTestBApprovalTxHash = await erc20Token
+    //     .approve(contractWrappers.contractAddresses.erc20Proxy, UNLIMITED_ALLOWANCE_IN_BASE_UNITS)
+    //     .sendTransactionAsync({ from: maker, gasPrice: 0 });
+    // console.log(makerTestBApprovalTxHash)
 
+    // const etherToken = new ERC20TokenContract(TestCTokenAddress, providerEngine());
+    // const takerTestCApprovalTxHash = await etherToken
+    //     .approve(contractWrappers.contractAddresses.erc20Proxy, UNLIMITED_ALLOWANCE_IN_BASE_UNITS)
+    //     .sendTransactionAsync({ from: taker, gasPrice: 0 });
+    // console.log(takerTestCApprovalTxHash)
 
-    const etherToken = new ERC20TokenContract(TestCTokenAddress, providerEngine());
-    const takerTestCApprovalTxHash = await etherToken
-        .approve(contractWrappers.contractAddresses.erc20Proxy, UNLIMITED_ALLOWANCE_IN_BASE_UNITS)
-        .sendTransactionAsync({ from: taker, gasPrice: 0 });
+    // const wethToken = new ERC20TokenContract("0x99c2f0c10a64fdbf466a5f4f24794f7c112c23a4".toLowerCase(), providerEngine());
+    // const takerWETHApprovalTxHash = await wethToken
+    //     .approve(contractWrappers.contractAddresses.staking, UNLIMITED_ALLOWANCE_IN_BASE_UNITS)
+    //     .sendTransactionAsync({ from: taker, gasPrice: 0 });
+    // console.log(takerWETHApprovalTxHash);
 
+    // Convert ETH into WETH for taker by depositing ETH into the WETH contract
+    // const takerWETHDepositTxHash = await contractWrappers.weth9.deposit().sendTransactionAsync({
+    //     value: takerAssetAmount,
+    //     from: taker,
+    // });
+    // console.log(takerWETHDepositTxHash)
 
     // Set up the Order and fill it
     const randomExpiration = utils.getRandomFutureDateInSeconds();
@@ -70,17 +83,105 @@ let utils = require('./utils');
         console.log("Fillable")
     }
 
-    txHash = await contractWrappers.exchange
-        .fillOrder(signedOrder, takerAssetAmount, signedOrder.signature)
-        .sendTransactionAsync({
-            from: taker,
-            ...TX_DEFAULTS,
-            value: utils.calculateProtocolFee([signedOrder]),
-        });
-    console.log(txHash)
+    // let res = await contractWrappers.exchange.setProtocolFeeCollectorAddress(NULL_ADDRESS).awaitTransactionSuccessAsync({ from: maker });
+    // console.log(res)
+    // txHash = await contractWrappers.exchange
+    //     .fillOrder(signedOrder, takerAssetAmount, signedOrder.signature)
+    //     .awaitTransactionSuccessAsync({
+    //         from: taker,
+    //         gas: 8000000,
+    //         gasPrice: 0,
+    //         value: 0
+    //     });
+    // console.log(txHash)
     providerEngine().stop();
 
 })().catch(err => {
     console.log('!!!!!!!!!!!!!!!!!!!', err)
 })
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // const stakingContract = contractWrappers.staking;
+    // let res = await stakingContract.addExchangeAddress(contractWrappers.contractAddresses.exchange).awaitTransactionSuccessAsync({
+    //     from: maker,
+    // });
+    // console.log(res)
+    // const tx = await stakingContract.removeExchangeAddress(contractWrappers.contractAddresses.exchange).awaitTransactionSuccessAsync({
+    //     from: maker,
+    // })
+    // console.log(tx)
+
+    // const receipt = await stakingContract
+    //     .payProtocolFee(maker, taker, new BigNumber(1))
+    //     .awaitTransactionSuccessAsync({ from: maker, value: 0 });
+    // console.log(receipt)
